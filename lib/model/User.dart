55bb69785e9132f647240fde1b401abf6c8ff19f
damien_it17121248 <it17121248@my.sliit.lk>
2020-04-29 23:24:51 +0530

    AuthService Created

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -1,23 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class User {
-  String name;
-  String password;
-  List<DocumentReference> trips;
-  DocumentReference reference;
+  final String userId;
 
-  User({this.name});
-
-  User.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        name = map['name'],
-        assert(map['trips'] != null),
-        trips = map['trips'];
-
-  User.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  toJson() {
-    return {'name': name, 'password': password};
-  }
+  User({this.userId});
 }
